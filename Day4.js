977dec5b995527860b95b9dcfdf5102246078756
Satyam <satyamsingh26002@gmail.com>
2026-07-15 20:52:08 +0530

    checkTriangle

---
+++ Day4.js
@@ -0,0 +1,30 @@
+//*----------------------------------------------------------------
+
+//*  Interview Question:
+
+//*----------------------------------------------------------------
+
+//? Write a function called checkTriangleType that takes three parameters representing the
+//  lengths of the sides of a triangle. The function should return a string indicating the type
+//  of triangle: "equilateral", "isosceles", or "scalene".
+const checkTriangleType=(a,b,c)=>{
+    if(a==b && b==c){
+        return "equilateral Triangle";
+    }
+    else if(a==b || b==c || a==c){
+        return "isocles triangle"
+    }
+    else {
+        return "scalene";
+    }
+    
+}
+console.log(checkTriangleType(3, 3, 3)); // Output: "equilateral"
+console.log(checkTriangleType(3, 6, 3)); // Output: "isosceles"
+console.log(checkTriangleType(5, 8, 6)); // Output: "scalene"
+
+//todo The function should adhere to the following rules:
+
+//? If all three sides are of equal length, return "equilateral".
+//? If only two sides are of equal length, return "isosceles".
+//? If all three sides have different lengths, return "scalene".
