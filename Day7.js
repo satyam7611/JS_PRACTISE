e747419319b0f3fd85989ec12139910962df6a86
Satyam <satyamsingh26002@gmail.com>
2026-07-16 10:00:00 +0530

    finding the max in array

---
+++ Day7.js
@@ -0,0 +1,20 @@
+// Write a function findMax that takes an array of numbers
+// as input and returns the maximum number in the array.
+
+
+const findMax=(arr)=>{
+    // const newArr=arr.sort((a,b)=>b-a)  // SORT IN DESCENDING
+    // return newArr[0];
+    let max=arr[0];
+    for(let i=0;i<arr.length;i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+// Example usage:
+console.log(findMax([1, 5, 3, 9, 2]));        // Output: 9
+console.log(findMax([-10, -5, -3, -9, -2])); // Output: -2
+console.log(findMax([5]));                    // Output: 5
