da67753fd3d495ae2ef4d54cfb1df8387aef3396
Satyam <satyamsingh26002@gmail.com>
2026-08-13 09:06:20 +0530

    .\Day21.js

---
+++ Day21.js
@@ -0,0 +1,14 @@
+//  write a funciton to calculate the mean of an elements of an array
+
+
+const mean=(nums)=>{
+const ans=nums.reduce((acc,currEle)=>{
+     return acc+=currEle
+},0)
+  return nums.length===0?0:ans/nums.length
+}
+
+console.log(mean([1,2,3,4,5,6]))
+console.log(mean([10,20,30]))
+console.log(mean([-1,0,1]))
+console.log(mean([]))
