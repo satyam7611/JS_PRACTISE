4d7baa87a1ff9c107e01ee14d3ecb7d4746bd603
Satyam <satyamsingh26002@gmail.com>
2026-08-14 21:37:47 +0530

    .\Day22.js

---
+++ Day22.js
@@ -0,0 +1,13 @@
+//  Write a function to count the occurrence of a number and store it in a object
+
+const countOccur = (arr) => {
+    let obj = {};
+
+    for (let elements of arr) {
+        obj[elements] = (obj[elements] || 0) + 1;
+    }
+
+    return obj;
+};
+
+console.log(countOccur([1, 2, 2, 3, 1, 4, 2]));
